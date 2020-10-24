5d34455cc810609a4dc5ce74d56bfeaa282d6f5b
msolorio <msolorio.github@gmail.com>
2020-10-24 15:13:23 -0700

    adds solution for 10 has consecutive items

---
+++ codepeace/10_has_consecutive_items.py
@@ -0,0 +1,10 @@
+"""
+You are given a list of unique integers arr, and two integers a and b. Your task is to find
+out whether or not a and b appear consecutively in arr, and return a boolean value (True if
+a and b are consecutive, False otherwise). It is guaranteed that a and b are both present in arr.
+"""
+def has_consecutive_items(list, item_a, item_b):
+    item_a_index = list.index(item_a)
+    item_b_index = list.index(item_b)
+
+    return abs(item_a_index - item_b_index) == 1
