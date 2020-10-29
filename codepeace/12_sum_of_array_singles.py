d2b3aae7b89d96734c9fa81c590a847503569c89
msolorio <msolorio.github@gmail.com>
2020-10-28 22:00:44 -0700

    adds solution for sum of array singles

---
+++ codepeace/12_sum_of_array_singles.py
@@ -0,0 +1,19 @@
+"""
+In this Kata, you will be given an array of numbers in which two numbers occur once and the rest occur only twice. Your task will be to return the sum of the numbers that occur only once.
+
+For example, repeats([4,5,7,5,4,8]) = 15 because only the numbers 7 and 8 occur once, and their sum is 15. Every other number occurs twice.
+
+More examples in the test cases.
+
+Good luck!
+"""
+def get_sum_of_singles(list):
+    return sum([num for num in list if list.count(num) == 1])
+
+# def get_sum_of_singles(list):
+#     sum = 0
+#     for num in list:
+#         if list.count(num) == 1:
+#             sum += num
+
+#     return sum
